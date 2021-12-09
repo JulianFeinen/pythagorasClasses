bf8f6211a8eaff085547cdd6a8d8f29e2b9d84b0
baitwell <julian.feinen@gmail.com>
2021-12-09 18:44:15 +0100

    ff

---
+++ js/Dreieck.js
@@ -0,0 +1,24 @@
+class Dreieck {
+    constructor(aLaenge, bLaenge, cLaenge, doppelF) {
+        this.aLaenge = aLaenge;
+        this.bLaenge = bLaenge;
+        this.cLaenge = cLaenge;
+    }
+
+    fKathetenBerechnen() {
+        return this.cLaenge = Math.sqrt((this.aLaenge * this.aLaenge) + (this.bLaenge * this.bLaenge));
+    }
+
+    FlaechenRechnung() {
+        return this.doppelF = (this.aLaenge * this.bLaenge) / 2;
+    }
+
+    fHypotenuseUndABerechnen() {
+        return this.bLaenge = Math.sqrt((this.cLaenge * this.cLaenge) - (this.aLaenge * this.aLaenge));
+    }
+
+    fHypotenuseUndBBerechnen() {
+        return this.aLaenge = Math.sqrt((this.cLaenge * this.cLaenge) - (this.cLaenge * this.bLaenge));
+    }
+
+}
--- js/pythagorasClassesScript.js
+++ js/pythagorasClassesScript.js
@@ -1,67 +1,93 @@
 
-class Dreieck
+let dreieckobj = new Dreieck();
+
+function setCookie(cName,cValue,exdays)
 {
-    constructor(aLaenge,bLaenge,cLaenge,doppelF)
-    {
-        this.aLaenge = aLaenge;
-        this.bLaenge = bLaenge;
-        this.cLaenge = cLaenge;
-    }
-
-    fKathetenBerechnen()
-    {   
-    return this.cLaenge = Math.sqrt((this.aLaenge*this.aLaenge) + (this.bLaenge*this.bLaenge));
-    }
-
-    FlaechenRechnung() 
-    {
-    return this.doppelF = (this.aLaenge * this.bLaenge)/ 2;
-    }
-
-    fHypotenuseUndABerechnen()
-    {
-    return this.bLaenge = Math.sqrt((this.cLaenge * this.cLaenge) - (this.aLaenge * this.aLaenge));
-    }
-
-    fHypotenuseUndBBerechnen()
-    {
-    return this.aLaenge = Math.sqrt((this.cLaenge * this.cLaenge) - (this.cLaenge * this.bLaenge));
-    }
-
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
 }
-
-
+function getCookie(cName)//mein Ansatz aLaenge=129.33676971379793; bLaenge=13; cLaenge=136
+{
+    let cN = cName;
+    let allCookies = document.cookie.split(";");
+    for(let i = 0; i<allCookies.length;i++)
+    {
+        if(allCookies[i].includes(cN))
+        {
+            let temp = allCookies[i].replace(cN+"=", "");
+            return temp;
+        }
+    }
+}
+function mainLoaded()
+{
+    document.getElementById("inputA").value = getCookie("aLaenge");
+    document.getElementById("inputB").value = getCookie("bLaenge");
+    document.getElementById("inputC").value = getCookie("cLaenge");
+}
 
 function Berechnen()
 {
     if(isKathetenInputValid()) 
-    {
-        let dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
-        
+    {  
+         dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
+       
         document.getElementById("inputC").value = dreieckobj.fKathetenBerechnen();
         document.getElementById("labelF").innerHTML = "(Fläche)F:" + (dreieckobj.FlaechenRechnung());
+        setAllCookies(dreieckobj);
     }
 
     if(isHypotenusenUndAInputValid())
     {
-        let dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
-
+         dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
+        if(dreieckobj.cLaenge>dreieckobj.aLaenge)
+        {
         document.getElementById("inputB").value = dreieckobj.fHypotenuseUndABerechnen();
         document.getElementById("labelF").innerHTML = "(Fläche)F:" + (dreieckobj.FlaechenRechnung());
+        }
+        else
+        {
+            alleInputsResetten()
+            alert("Fehler: Die Kathetenlänge darf nicht höher als die Hypotenusenlänge sein.");
+        }
+        setAllCookies(dreieckobj);
     }
 
     if(isHypotenusenUndBInputValid())
     {
-        let dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
+         dreieckobj = new Dreieck(document.getElementById("inputA").value, document.getElementById("inputB").value, document.getElementById("inputC").value);
 
+        if(dreieckobj.cLaenge>dreieckobj.bLaenge)
+        {
         document.getElementById("inputA").value = dreieckobj.fHypotenuseUndBBerechnen();
         document.getElementById("labelF").innerHTML = "(Fläche)F:" + (dreieckobj.FlaechenRechnung());
+        }
+        else
+        {
+            alleInputsResetten()
+            alert("Fehler: Die Kathetenlänge darf nicht höher als die Hypotenusenlänge sein.");
+        }
+        setAllCookies(dreieckobj);
+    }
+    let abstract = "a:" + dreieckobj.aLaenge + ", b:" + dreieckobj.bLaenge + ", c:" + dreieckobj.cLaenge;
+    
+    if((typeof dreieckobj.aLaenge !=="undefined") ||(typeof dreieckobj.bLaenge !=="undefined") || (typeof dreieckobj.cLaenge !=="undefined"))
+    {
+        document.getElementById("liste").innerHTML = "<li>" + abstract +"</li>" + document.getElementById("liste").innerHTML;
     }
 }
 
 
 
 
+
+function setAllCookies(dreieckobj) {
+    setCookie("aLaenge", dreieckobj.aLaenge, 2);
+    setCookie("bLaenge", dreieckobj.bLaenge, 2);
+    setCookie("cLaenge", dreieckobj.cLaenge, 2);
+}
 
 function isHypotenusenUndBInputValid() {
     return (document.getElementById("inputB").value !== "") && (document.getElementById("inputC").value !== "") && (document.getElementById("inputA").value == "");
@@ -77,7 +103,6 @@ function isKathetenInputValid() {
 
 function alleInputsResetten()
 {
-   
     document.getElementById("inputA").value = "";
     document.getElementById("inputB").value = "";
     document.getElementById("inputC").value = "";
